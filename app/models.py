25536943c1cea4dac238d39b065adaef525ba0b6
Macbook <macbook@MacBook-Pro-Macbook.local>
2026-07-12 18:51:21 +0900

    Fix created_at default and working API

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime, timezone
 
 
 class Url(db.Model):
@@ -7,5 +8,5 @@ class Url(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.Text, nullable=False)
     short_code = db.Column(db.String(10), nullable=False, unique=True)
-    created_at = db.Column(db.DateTime(timezone=True), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
 
